ddf14614111e2bc8169dd9f387d0eec4b0fd1676
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-03-18 07:10:06 +0700

    first commit

---
+++ src/Portofolio-Website-Fahri/Zulkarnaenfhr.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Zulkarnaenfhr extends Component {
+    render() {
+        return (
+            <div>
+                <h1>masuk</h1>
+            </div>
+        );
+    }
+}
+
+export default Zulkarnaenfhr;
