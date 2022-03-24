8b12c0acbd8153ec4cf66775ce4abdaac7cffaa5
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-03-24 10:01:13 +0700

    hapusmata

--- src/Portofolio-Website-Fahri/Zulkarnaenfhr.jsx
+++ src/Portofolio-Website-Fahri/Zulkarnaenfhr.jsx
@@ -11,36 +11,6 @@ import Getintouch from "./Container/6. GetInTouch/GetInTouch";
 import Signiaturesection from "./Container/7. Signiature Section/SigniatureSection";
 
 class Zulkarnaenfhr extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            first: "",
-        };
-        this.handleMouseMoveforEye = this.handleMouseMoveforEye.bind(this);
-    }
-    handleMouseMoveforEye = () => {
-        const container = document.querySelector(".starsbackground");
-        container.addEventListener("mousemove", (e) => {
-            const eyes = document.querySelectorAll(".eye");
-            [].forEach.call(eyes, function (eye) {
-                // Get the mouse position on the horizontal axis
-                let mouseX = eye.getBoundingClientRect().right;
-                // If it's the left eye we need the left offset instead the right
-                if (eye.classList.contains("eye-left")) {
-                    mouseX = eye.getBoundingClientRect().left;
-                }
-                // Now we also need the vertical offset
-                let mouseY = eye.getBoundingClientRect().top;
-                // Now we are going to calculate the radian value of the offset between the mouse and the eye
-                let radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
-                // Let's convert this into a degree based value so we can use it in CSS
-                let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
-                // Now all we have to do is add this degrees to our eye!
-                eye.style.transform = `rotate(${rotationDegrees}deg)`;
-            });
-        });
-    };
     render() {
         return (
             <div onMouseMove={this.handleMouseMoveforEye} className="starsbackground">
