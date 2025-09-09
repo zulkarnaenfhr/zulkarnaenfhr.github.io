db07a45e983ba991d3ee886b82670051921c54ad
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2025-09-09 19:02:37 +0700

    changes deployment

--- src/Portofolio-Website-Fahri/Container/1. Navbar/Navbar.jsx
+++ src/Portofolio-Website-Fahri/Container/1. Navbar/Navbar.jsx
@@ -3,48 +3,48 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <section>
-                <div data-aos="fade-left" data-aos-offset="100" id="fixedSideNavbar" data-aos-delay="2500" className="navbarfahri">
-                    <div>
-                        <ul className="ulmenu">
-                            <li>
-                                <a aria-label="navbarHomeSection" href="#homeSection">
-                                    / Home
-                                </a>
-                            </li>
-                            <li>
-                                <a aria-label="navbarAboutMeSection" href="#aboutMeSection">
-                                    / About Me
-                                </a>
-                            </li>
-                            <li>
-                                <a aria-label="navbarExperienceSection" href="#experienceSection">
-                                    / Experience
-                                </a>
-                            </li>
-                            <li>
-                                <a aria-label="navbarProjectSection" href="#projectSection">
-                                    / Project
-                                </a>
-                            </li>
-                            <li>
-                                <a aria-label="navbarGetInTouch" href="#getInTouch">
-                                    / Get In Touch
-                                </a>
-                            </li>
-                            <li>
-                                <Link to="/files/CV_Fahri Izzuddin Zulkarnaen_Website Developer.pdf" target="_blank" download="CV_Fahri izzuddin Zulkarnaen_Website Developer">
-                                    <p className="navbarDownloadCV">/ Download CV</p>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+   render() {
+      return (
+         <section>
+            <div data-aos="fade-left" data-aos-offset="100" id="fixedSideNavbar" data-aos-delay="2500" className="navbarfahri">
+               <div>
+                  <ul className="ulmenu">
+                     <li>
+                        <a aria-label="navbarHomeSection" href="#homeSection">
+                           / Home
+                        </a>
+                     </li>
+                     <li>
+                        <a aria-label="navbarAboutMeSection" href="#aboutMeSection">
+                           / About Me
+                        </a>
+                     </li>
+                     <li>
+                        <a aria-label="navbarExperienceSection" href="#experienceSection">
+                           / Experience
+                        </a>
+                     </li>
+                     <li>
+                        <a aria-label="navbarProjectSection" href="#projectSection">
+                           / Project
+                        </a>
+                     </li>
+                     <li>
+                        <a aria-label="navbarGetInTouch" href="#getInTouch">
+                           / Get In Touch
+                        </a>
+                     </li>
+                     <li>
+                        <Link to="/files/CV_Fahri Izzuddin Zulkarnaen_Website Developer.pdf" target="_blank" download="CV_Fahri izzuddin Zulkarnaen_Website Developer">
+                           <p className="navbarDownloadCV">/ Download CV</p>
+                        </Link>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </section>
+      );
+   }
 }
 
 export default Navbar;
