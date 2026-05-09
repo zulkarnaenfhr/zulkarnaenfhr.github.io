25bfbc5b9484417d72c87ec7f6948cd8e7f2d53b
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2026-05-09 19:31:25 +0700

    update about me section

--- src/Portofolio-Website-Fahri/Container/2. HomeSection/HomeSection.jsx
+++ src/Portofolio-Website-Fahri/Container/2. HomeSection/HomeSection.jsx
@@ -1,65 +1,85 @@
-import React, { Component } from "react";
-import "./HomeSection.css";
+import React, { Component } from 'react';
+import './HomeSection.css';
 
 class Homesection extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            windowWidth: 0,
-        };
-    }
-    componentDidMount() {
-        this.setState({
-            windowWidth: window.innerWidth,
-        });
-    }
-    render() {
-        return (
-            <section id="homeSection">
-                <div className="container-fluid hai">
-                    <div className="container hai-content">
-                        <div className="nama">
-                            {this.state.windowWidth > 768 ? (
-                                <h1 className="penulisanNama" data-aos-delay="2500" data-aos="fade-up">
-                                    Hi,{" "}
-                                    <span id="fahri" style={{ color: "cadetblue" }}>
-                                        Your <span className="zulkarnaen">SpaceCapt</span>{" "}
-                                    </span>{" "}
-                                    Here :)
-                                </h1>
-                            ) : (
-                                <h1 className="penulisanNama" data-aos-delay="2500" data-aos="fade-up">
-                                    Hi,{" "}
-                                    <span id="fahri" style={{ color: "cadetblue" }}>
-                                        Fahri <span className="zulkarnaen">SpaceCapt</span>{" "}
-                                    </span>{" "}
-                                    Here :)
-                                </h1>
-                            )}
+		this.state = {
+			windowWidth: 0,
+		};
+	}
+	componentDidMount() {
+		this.setState({
+			windowWidth: window.innerWidth,
+		});
+	}
+	render() {
+		return (
+			<section id="homeSection">
+				<div className="container-fluid hai">
+					<div className="container hai-content">
+						<div className="nama">
+							{this.state.windowWidth > 768 ? (
+								<h1
+									className="penulisanNama"
+									data-aos-delay="2500"
+									data-aos="fade-up">
+									Hi,{' '}
+									<span
+										id="fahri"
+										style={{ color: 'cadetblue' }}>
+										Your <span className="zulkarnaen">SpaceCapt</span>{' '}
+									</span>{' '}
+									Here :)
+								</h1>
+							) : (
+								<h1
+									className="penulisanNama"
+									data-aos-delay="2500"
+									data-aos="fade-up">
+									Hi,{' '}
+									<span
+										id="fahri"
+										style={{ color: 'cadetblue' }}>
+										Fahri <span className="zulkarnaen">SpaceCapt</span>{' '}
+									</span>{' '}
+									Here :)
+								</h1>
+							)}
 
-                            <div data-aos-delay="2500" data-aos="fade-up" className="eye-container" >
-                                {/* <div className="eye"></div>
+							<div
+								data-aos-delay="2500"
+								data-aos="fade-up"
+								className="eye-container">
+								{/* <div className="eye"></div>
                                 <div className="eye2"></div> */}
-                                <div className="eye eye-left">
-                                    <div className="eye-inner"></div>
-                                </div>
-                                <div className="eye eye2 eye-right">
-                                    <div className="eye-inner"></div>   
-                                </div>
-                            </div>
-                        </div>
-                        <h1 className="bawahNama" data-aos-delay="2750" data-aos="fade-up">
-                            I Create stuff that all i love, <span style={{ color: "#19f9d8" }}>sometimes!</span>
-                        </h1>
-                        <h2 id="bawahnya" className="whoAmI" data-aos-delay="3000" data-aos="fade-up">
-                            I am a graduate with a profound interest in Fullstack Development, having previously explored Machine Learning, Computer Vision, and everything in between.{" "}
-                        </h2>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+								<div className="eye eye-left">
+									<div className="eye-inner"></div>
+								</div>
+								<div className="eye eye2 eye-right">
+									<div className="eye-inner"></div>
+								</div>
+							</div>
+						</div>
+						<h1
+							className="bawahNama"
+							data-aos-delay="2750"
+							data-aos="fade-up">
+							I Create stuff that all i love, <span style={{ color: '#19f9d8' }}>sometimes!</span>
+						</h1>
+						<h2
+							id="bawahnya"
+							className="whoAmI"
+							data-aos-delay="3000"
+							data-aos="fade-up">
+							Fullstack Software Developer with 5 years of experience building scalable web applications. Skilled in problem-solving, detail-oriented, and able to work independently or in teams to deliver high-quality solutions.
+						</h2>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Homesection;
--- src/Portofolio-Website-Fahri/Container/3. AboutMeSection/AboutMe.jsx
+++ src/Portofolio-Website-Fahri/Container/3. AboutMeSection/AboutMe.jsx
@@ -1,73 +1,98 @@
-import React, { Component } from "react";
-import "./AboutMe.css";
+import React, { Component } from 'react';
+import './AboutMe.css';
 
 class Aboutme extends Component {
-   constructor(props) {
-      super(props);
+	constructor(props) {
+		super(props);
 
-      this.state = {
-         innerWidth: 0,
-      };
-   }
-   componentDidMount() {
-      this.setState({
-         innerWidth: window.innerWidth,
-      });
-   }
-   render() {
-      return (
-         <section id="aboutMeSection">
-            <div className="container-fluid aboutMe">
-               <div className="container">
-                  <h1 className="sectionTitle" data-aos="fade-up" data-aos-offset="100">
-                     / About me
-                  </h1>
-                  <div className="col-10 aboutMeContent" data-aos="fade-up">
-                     <div>
-                        <div id="aboutMeLeft" className={this.state.innerWidth > 992 ? "col-3 aboutMeContentLeft" : "aboutMeContentLeft"} data-aos="fade-up" data-aos-delay="50"></div>
-                        <div id="aboutMeRight" className={this.state.innerWidth > 992 ? "col-9 aboutMeDescription" : "aboutMeDescription"}>
-                           <div className="paragraf paragraf1" data-aos="fade-up" data-aos-delay="100">
-                              Hiiii, my name is <b className="boldAboutMe">Fahri Izzuddin Zulkarnaen</b>. I graduated from Informatics Engineering at <b className="boldAboutMe">UPN Veteran Jawa Timur</b> with a GPA of 3.90, specializing in
-                              Fullstack Development and Artificial Intelligence Engineering. I have also worked as an IT Specialist at <b className="boldAboutMe">PT Bank Central Indonesia</b>.
-                           </div>
-                           <div className="paragraf paragraf2" data-aos="fade-up" data-aos-delay="100">
-                              I am passionate about building excellent software that improves the lives of those around me. I am diving deeper into
-                              <b className="boldAboutMe"> Front-End Development</b>. focusing on creating and animating 3D computer graphics for web browsers. I'm also Well-organised person, problem solver, independent employee with high
-                              attention to detail.
-                           </div>
-                           <div className="paragraf paragraf2" data-aos="fade-up" data-aos-delay="100">
-                              In my free time, I continue to learn and explore advancements in <b className="boldAboutMe">Artificial Intelligence</b> .
-                           </div>
+		this.state = {
+			innerWidth: 0,
+		};
+	}
+	componentDidMount() {
+		this.setState({
+			innerWidth: window.innerWidth,
+		});
+	}
+	render() {
+		return (
+			<section id="aboutMeSection">
+				<div className="container-fluid aboutMe">
+					<div className="container">
+						<h1
+							className="sectionTitle"
+							data-aos="fade-up"
+							data-aos-offset="100">
+							/ About me
+						</h1>
+						<div
+							className="col-10 aboutMeContent"
+							data-aos="fade-up">
+							<div>
+								<div
+									id="aboutMeLeft"
+									className={this.state.innerWidth > 992 ? 'col-3 aboutMeContentLeft' : 'aboutMeContentLeft'}
+									data-aos="fade-up"
+									data-aos-delay="50"></div>
+								<div
+									id="aboutMeRight"
+									className={this.state.innerWidth > 992 ? 'col-9 aboutMeDescription' : 'aboutMeDescription'}>
+									<div
+										className="paragraf paragraf1"
+										data-aos="fade-up"
+										data-aos-delay="100">
+										Hi! I’m <b className="boldAboutMe">Fahri Izzuddin Zulkarnaen</b>, a Fullstack Software Developer with experience building scalable and user-focused web applications. I graduated from Informatics Engineering at
+										UPN Veteran Jawa Timur with a GPA of 3.90 and currently work as an IT Specialist at <b className="boldAboutMe">PT Bank Central Indonesia(bca)</b>.
+									</div>
+									<div
+										className="paragraf paragraf2"
+										data-aos="fade-up"
+										data-aos-delay="100">
+										My experience includes developing high-impact applications used by thousands of users, maintaining microservices architectures, and supporting enterprise-scale system integrations. I mainly work with
+										<b className="boldAboutMe"> Angular, Java Spring Boot, React, and Node.js across both frontend and backend development.</b>
+										<br />
+									</div>
+									<div
+										className="paragraf paragraf2"
+										data-aos="fade-up"
+										data-aos-delay="100">
+										Outside of work, I actively explore advancements in Artificial Intelligence and software engineering to keep expanding my technical knowledge and perspective.
+									</div>
 
-                           <div data-aos="fade-up" data-aos-delay="300" data-aos-offset="100">
-                              <div className="technology">Here are some technologies I have been working with:</div>
-                              <div className="row techlist-content" style={{ marginLeft: "-1.5rem" }}>
-                                 <div className="col-6 technologylist">
-                                    <ul>
-                                       <li>Python</li>
-                                       <li>Java Spring Boot</li>
-                                       <li>Java Spring Batch</li>
-                                       <li>Angular Js</li>
-                                    </ul>
-                                 </div>
-                                 <div className="col-6 technologylist">
-                                    <ul>
-                                       <li>Next and React Js</li>
-                                       <li>Node Js</li>
-                                       <li>SQL Databases</li>
-                                       <li>NoSQL Databases</li>
-                                    </ul>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </section>
-      );
-   }
+									<div
+										data-aos="fade-up"
+										data-aos-delay="300"
+										data-aos-offset="100">
+										<div className="technology">Here are some technologies I have been working with:</div>
+										<div
+											className="row techlist-content"
+											style={{ marginLeft: '-1.5rem' }}>
+											<div className="col-6 technologylist">
+												<ul>
+													<li>Python</li>
+													<li>Java Spring Boot</li>
+													<li>Java Spring Batch</li>
+													<li>Angular Js</li>
+												</ul>
+											</div>
+											<div className="col-6 technologylist">
+												<ul>
+													<li>Next and React Js</li>
+													<li>Node Js</li>
+													<li>SQL Databases</li>
+													<li>NoSQL Databases</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Aboutme;
